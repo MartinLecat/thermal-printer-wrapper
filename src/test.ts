e60aaddb0e5341bda8ba924699acaf5c03c2c3b9
Martin Lecat <martinlecat51@gmail.com>
2024-09-01 11:57:03 +0200

    test: Created a practical test file

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,20 @@
 import { ThermalPrinterWrapper } from "./ThermalPrinterWrapper.js";
+import net from "net";
 
+const client = new net.Socket();
 const tpw = new ThermalPrinterWrapper();
-tpw.text("a");
-console.log(tpw.getQueue());
+client.connect(9100, "192.168.0.3", function (...args) {
+    console.log("Connected", args);
+    const result = tpw.initialize().line("Ahahahahah").cut().encode();
+    console.log("Sending", result);
+    client.write(result, function (...args) {
+        console.log("Sent", args);
+        client.end();
+    });
+});
+
+client.on("data", function (data) {
+    data.forEach((v) => {
+        console.log("Received", v.toString(16));
+    });
+});
